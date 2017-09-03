0b0da5b62ed2d73e4e89e5bc2b79799ad58305dd
anay-jain <anay9999682713@gmail.com>
2017-09-03 11:46:44 +0530

    added user_profile model

--- user_profile/models.py
+++ user_profile/models.py
@@ -4,6 +4,36 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+from .conf import *
+
+class user_profile(User):
+    username = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30 , blank=True)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    is_superuser = False
+    gender= models.CharField(max_length=10 , choices=GENDER)
+    # email_regex=RegexValidator(email_regex=r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+    Stream = models.CharField(max_length= 100 , choices=STREAM)
+    year = models.CharField(max_length=10 , choices=YEAR)
+
+    #optional
+    user_domain= models.CharField(max_length=200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
