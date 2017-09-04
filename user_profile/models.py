76199237bd25530f512087935761160100227c40
Jatin <jatin.katyal13@gmail.com>
2017-09-04 09:54:45 +0530

    Code shortened

--- user_profile/models.py
+++ user_profile/models.py
@@ -9,12 +9,6 @@ from django.core.validators import RegexValidator
 from .conf import *
 
 class user_profile(User):
-    username = models.CharField(max_length=150)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30 , blank=True)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    is_superuser = False
     gender= models.CharField(max_length=10 , choices=GENDER)
     # email_regex=RegexValidator(email_regex=r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
     Stream = models.CharField(max_length= 100 , choices=STREAM)
